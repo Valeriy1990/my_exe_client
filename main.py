176824c8614b15d7c5017fea725e045ed8e320f6
Valeriy <v.bekreev@mail.ru>
2025-05-21 17:44:05 +0300

    Скрин Main теперь сохраняет состояние приложений. Другие скрины нет

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from kivymd.uix.textfield import MDTextField
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.screenmanager import MDScreenManager
 from kivymd.uix.navigationrail import MDNavigationRail, MDNavigationRailItem
+import json
+import cProfile
 
 import asyncio
 from asyncio import sleep
@@ -42,8 +44,6 @@ logger = logging.getLogger(__name__)
 
 class MainApp(MDApp):
     '''Здесь я создаю главный скрин'''
-    access = True # Поставить на False
-    login_active = 'test_login_in_main'
     
     def build(self):
         self.theme_cls.theme_style = "Dark"
@@ -54,7 +54,7 @@ class MainApp(MDApp):
                                     on_press=self.account_reset,
                                     text="Account Reset"),               
                                     md_bg_color=(0.4, 0.4, 0.4, 1),
-                                    current_selected_item=1,
+                                    current_selected_item=-1,
                                     size_hint = (None, None))
         panel.height = 17 * 56 + 20
     
@@ -62,7 +62,7 @@ class MainApp(MDApp):
         self.text_login = MDTextField(size_hint=(0.4, None),
                                  pos_hint={"center_x": 0.5, "center_y": 0.8},
                                  mode="rectangle",
-                                 hint_text="Никнейм",
+                                 hint_text="Login",
                                  hint_text_color_normal = blue,
                                  icon_left_color_normal = blue,
                                  max_text_length=12,
@@ -73,14 +73,14 @@ class MainApp(MDApp):
                                  cursor_blink=True,
                                  icon_left="account",
                                  foreground_color=green,
-                                #  on_text_validate=self.next_field,
+                                 on_text_validate=self.next_field,
                                  fill_color_normal=green)
         
         '''Лейбл пароль'''
         self.text_passrd = MDTextField(size_hint=(0.4, None),
                                   pos_hint={"center_x": 0.5, "center_y": 0.68},
                                   mode="rectangle",
-                                  hint_text="Пароль",
+                                  hint_text="Password",
                                   icon_left="lock-outline",
                                   hint_text_color_normal = blue,
                                   icon_left_color_normal = blue,
@@ -93,7 +93,8 @@ class MainApp(MDApp):
         self.text_field0 = MDTextField(size_hint=(0.4, None),
                                  pos_hint={"center_x": 0.35, "center_y": 0.93},
                                  mode="line",
-                                 hint_text="None",
+                                 hint_text=MainApp.data['info'],
+                                 text = '',
                                  hint_text_color_normal = my_color,
                                  icon_left_color_normal = my_color,
                                  max_text_length=16,
@@ -136,7 +137,8 @@ class MainApp(MDApp):
                                  allow_copy=False,
                                  base_direction="ltr",
                                  cursor_blink=True,
-                                 text='192.168.1.33',
+                                 text=MainApp.data['url'][:-5] if MainApp.data['url'] else '',
+                                #  text='192.168.1.33',
                                  icon_left="")
         
         '''Лейбл для порта'''
@@ -151,22 +153,17 @@ class MainApp(MDApp):
                                  allow_copy=False,
                                  base_direction="ltr",
                                  cursor_blink=True,
-                                 text='8066',
+                                 text=MainApp.data['url'][-4:] if MainApp.data['url'] else '',
+                                #  text='8066',
                                  icon_left="")
 
         '''Кнопка выхода из приложения'''
         exit_button = MDFloatingActionButton(icon="exit-run",
                                       md_bg_color = (0, 1, 0.7, 0.7),
                                       icon_color=(1, 1, 1, 1),
-                                      on_press=self.stop_program,
+                                      on_press=self.on_stop,
+                                    #   on_press=self.on_start,
                                       pos_hint={"center_x": 0.15, "center_y": 0.08})
-        
-        '''Кнопка подключения к серверу'''
-        server_button = MDFloatingActionButton(icon="connection",
-                                      md_bg_color = (0, 1, 0.7, 0.7),
-                                      icon_color=(1, 1, 1, 1),
-                                      on_press=self.on_server,
-                                      pos_hint={"center_x": 0.94, "center_y": 0.08})
 
         '''Создание пустого макета, не привязанного к экрану'''
         main_layout = MDBoxLayout(orientation="vertical")   
@@ -196,9 +193,8 @@ class MainApp(MDApp):
                         badge_icon_color=(1, 0, 0, 1))
             button.screen_name = f"Screen_{room}"
             panel.add_widget(button)
+        panel.add_widget(MDNavigationRailItem())  # чтобы панели по дефолту не включались
         
-        self.set_url(self.text_ip.text, self.text_port.text)  
-
         ml.add_widget(panel)
         main_screen.add_widget(ml)
        
@@ -210,22 +206,12 @@ class MainApp(MDApp):
         main_screen.add_widget(self.text_port) 
         
         main_screen.add_widget(confirm)
-        main_screen.add_widget(server_button)
         main_screen.add_widget(exit_button)
         main_screen.add_widget(eye_outline)
         main_screen.add_widget(main_layout)
         
         return self.sm  # Тут я возвращаю менедежер, что бы работать с ним
     
-    def set_url(self, ip, port):
-        '''Адрес сервера'''
-        self.url = f'{ip}:{port}' 
-    
-    def stop_program(self, instance):
-        '''Завершить приложение'''
-        logger.info('Успешный выход из программы. Ты супер!!')
-        App.get_running_app().stop()
-
     def next_field(self, instance):
         """Если текст логина введён в нужном формате, фокус сменяется на пароль"""
         logger.info('Смена фокуса с логина на пароль')
@@ -241,33 +227,37 @@ class MainApp(MDApp):
             self.text_passrd.password = True
             instance.icon = "eye-outline"
 
-    def on_confirm(self, instance):
+    def on_confirm(self, instance=None):
         '''Авторизация'''
 
         @mainthread
         def success(req, result):
             """В случае успеха"""
             logger.info(f'Сервер подтвердил запрос')
-            self.login_active = self.text_login.text
-            self.text_field0.hint_text = self.text_login.text if req._result else "Неверный логин или пароль"
+            if req._result:
+                MainApp.data['accses'] = req._result
+                self.save()
+                self.on_server()
+                self.text_field0.hint_text = MainApp.data['info']
+            else:
+                self.text_field0.hint_text = "Неверный логин или пароль"
             self.text_passrd.text = ""
             self.text_login.text = ""
-            MainApp.access = req._result
             logger.info(f'Процедура авторизации прошла успешно')
 
         @mainthread
         def on_error(req, error):
             """В случае неудачи"""
             self.text_field0.hint_text = "Ожидание ответа от сервера..."
-            MainApp.access = req._result
+            MainApp.data['accses'] = req._result
             load()
-            logger.debug(f'Ошибка авторизации') 
+            logger.debug(f'Сервер не отвечает') 
 
         @mainthread
         def failure(req, result):
             """Данные на сервер переданы, но есть проблема"""
             self.text_field0.hint_text = "Ошибка со стороны сервера"
-            MainApp.access = req._result
+            MainApp.data['accses'] = req._result
             logger.debug(f'Ошибка со стороны сервера') 
 
         def is_network_available():
@@ -282,11 +272,11 @@ class MainApp(MDApp):
 
         def load():
             """Get запрос с данными пользователя осуществляется здесь"""
-            self.set_url(self.text_ip.text, self.text_port.text)          
+            # self.set_url(self.text_ip.text, self.text_port.text)          
             logger.info(f'Попытка отправить запрос на сервер')    
             if is_network_available():
                 logger.error(f'Загрузка на сервер')
-                req = UrlRequest(url=f'http://{self.url}/avt/?login={self.text_login.text}&password={self.text_passrd.text}', 
+                req = UrlRequest(url=f'http://{MainApp.data['url']}/avt/?login={MainApp.data['login']}&password={MainApp.data['password']}', 
                                     on_success=success, 
                                     on_failure=failure,
                                     on_error=on_error)
@@ -294,6 +284,11 @@ class MainApp(MDApp):
                 logger.error(f'Интернета нет')
                 self.text_field0.hint_text = "Нет подключения к сети. Попробуем позже..."
                 Clock.schedule_once(lambda dt: load(), 5) # Повторная попытка через 5 секунд
+
+        MainApp.data['login'] = self.text_login.text
+        MainApp.data['password'] = self.text_passrd.text
+        MainApp.data['url'] = f'{self.text_ip.text}:{self.text_port.text}'   
+        MainApp.data['info'] = self.text_login.text
 
         load()  # Вложеная функции load внутри on_confirm        
 
@@ -303,9 +298,9 @@ class MainApp(MDApp):
         self.text_field0.hint_text = "Введите логин и пароль"
         MainApp.access = False
 
-    def on_server(self, instance):
+    def on_server(self, instance=None):
         """Проверка сервера в сети"""
-        self.set_url(self.text_ip.text, self.text_port.text)   
+        # self.set_url(self.text_ip.text, self.text_port.text)   
 
         def success(req, result):
             logger.info(f'Сервер прислал ответ: {req._result}')
@@ -320,19 +315,63 @@ class MainApp(MDApp):
             self.text_server.hint_text = 'Нет связи с сервером'
 
         logger.info(f'Проверка наличия сервера в сети')
-        response = UrlRequest(f'http://{self.url}/hello/', 
+        response = UrlRequest(f'http://{MainApp.data['url']}/hello/', 
                                   on_success=success, 
                                   on_failure=failur, 
                                   on_error=error)
         
-        Clock.schedule_once(lambda dt: self.on_server(f'http://{self.url}/hello/'), 5) # Повторная попытка через 5 секунд
+        Clock.schedule_once(lambda dt: self.on_server(f'http://{MainApp.data['url']}/hello/'), 5) # Повторная попытка через 5 секунд
 
     def to_scrn(self, instance):
         """Смена скрина"""
-        if MainApp.access == True:
-            self.__getattribute__(instance.screen_name).set_url(url=self.url, login=self.login_active)
+        if MainApp.data['accses']:
+            self.__getattribute__(instance.screen_name).set_url(url=MainApp.data['url'], login=MainApp.data['login'])
             self.sm.current = instance.screen_name  # Выбор экрана по имени            
 
+    def on_stop(self, instance):
+        '''Завершить приложение'''
+        logger.info('Успешный выход из программы. Ты супер!!')
+        # self.profile.disable()
+        # self.profile.dump_stats('my_exe_client/myapp.profile')
+        # self.save()
+        App.get_running_app().stop()
+
+    # def on_start(self):
+    #     logger.info('Функция on_start')
+    #     self.profile = cProfile.Profile()
+    #     self.profile.enable()
+        
+    def on_pause(self):
+       """Здесь вы можете сохранить данные, если это необходимо"""
+       self.save()
+       return True
+
+    def on_resume(self):
+       # Здесь вы можете проверить, нужно ли заменить какие-либо данные (обычно ничего не нужно)
+       pass
+
+    def save(self):
+        """Сохранение состояния приложения"""
+        # MainApp.data = {'accses': MainApp.access, 
+        #             'login': self.text_login.text if self.text_login.text else None,
+        #             'password': self.text_passrd.text if self.text_passrd.text else None,
+        #             'url' : self.url,
+        #             'info' : self.text_field0.text}
+        logger.info(f'Состояние файла данный: {MainApp.data}') 
+        with open('my_exe_client/data_client.json', 'w') as file:
+            json.dump(MainApp.data, file)  
+
+
 if __name__ == '__main__':
+    try:
+        with open('my_exe_client/data_client.json', 'r') as file:
+            MainApp.data = json.load(file)
+    except:
+        MainApp.data = {'accses': False, # Поставить на False
+                    'login': '',
+                    'password': '',
+                    'url' : None,
+                    'info' : 'Введите логин и пароль'}
+    logger.info(f'Состояние файла данный: {MainApp.data}')  
     MainApp().run()
 
